d2b2dc18391c12a07029574e468e3fc6551e791d
Federico <federicomolinachavez@gmail.com>
2025-10-04 07:39:25 -0500

    Update background colors in ResultsSection for improved visual consistency

--- src/components/ResultsSection.tsx
+++ src/components/ResultsSection.tsx
@@ -74,7 +74,7 @@ const ResultsSection: React.FC = () => {
 
               {/* Top Right - Bring first-time visitors back */}
               <div className="bg-blue-600 rounded-3xl p-8 text-white shadow-sm">
-                <div className="w-12 h-12 bg-white bg-opacity-10 rounded-lg flex items-center justify-center mb-4">
+                <div className="w-12 h-12 bg-blue-500 rounded-lg flex items-center justify-center mb-4">
                   <svg className="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20">
                     <path fillRule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clipRule="evenodd" />
                   </svg>
@@ -130,7 +130,7 @@ const ResultsSection: React.FC = () => {
 
             {/* Top Right - Bring first-time visitors back */}
             <div className="bg-blue-600 rounded-lg p-6 text-white shadow-sm">
-              <div className="w-10 h-10 bg-white bg-opacity-10 rounded-lg flex items-center justify-center mb-4">
+              <div className="w-10 h-10 bg-blue-500 rounded-lg flex items-center justify-center mb-4">
                 <svg className="w-5 h-5 text-white" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clipRule="evenodd" />
                 </svg>
